3733fc303ae8edf2493f284761d5d1136efac635
gordievvlad <gordievich12344@gmail.com>
2021-12-22 12:12:54 +0300

    Bugs fixed in generation.yml_v14

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'warehouse',
+    'warehouse_app.apps.WarehouseAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
